10f43d4e3ce1001ea30a94de200ab34d6d91d20b
niles87 <nbing19@gmail.com>
2020-03-13 10:18:51 -0700

    added file from burger time repo and edited for sequelize

---
+++ controllers/burger_controller.js
@@ -0,0 +1,41 @@
+var db = require("../models");
+module.exports = function(app) {
+  app.get("/", (req, res) => {
+    db.Burger.findAll({}).then(burger => {
+      res.render("home", {
+        burger: burger,
+      });
+    });
+  });
+
+  app.post("/api/burgers", (req, res) => {
+    db.Burger.create({
+      burger_name: req.body.burger_name,
+    }).then(function(burger) {
+      res.json(burger);
+    });
+  });
+
+  app.put("/api/burgers/:id", (req, res) => {
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    console.log(req.params.id);
+    console.log(req.body.devoured);
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    db.Burger.update(
+      {
+        devoured: req.body.devoured,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    )
+      .then(function(data) {
+        res.json(data);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+};

f56bd6f6ce4883ba462d8c217dc299cb69bff7d5
niles87 <nbing19@gmail.com>
2020-03-13 10:24:32 -0700

    file from burger time repo

---
+++ public/assets/javascript/burgers.js
@@ -0,0 +1,30 @@
+$(function() {
+  $(".devour").on("click", function(event) {
+    console.log($(this));
+    var id = $(this).data("id");
+    console.log(id);
+    var isDevoured = $(this).data("devoured");
+    console.log(isDevoured);
+    var devouredState = { devoured: isDevoured };
+
+    $.ajax("/api/burgers/" + id, { type: "PUT", data: devouredState }).then(function() {
+      console.log("changed devoured to", isDevoured);
+      // location.reload();
+    });
+  });
+
+  $(".create-burger").on("submit", function(event) {
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#burger")
+        .val()
+        .trim(),
+    };
+
+    $.ajax("/api/burgers", { type: "POST", data: newBurger }).then(function() {
+      console.log("created new burger");
+      location.reload();
+    });
+  });
+});

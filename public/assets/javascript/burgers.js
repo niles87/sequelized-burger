f3b00a064fa3b6ab72f7285c67a9ad2c2f8addbe
niles87 <nbing19@gmail.com>
2020-03-13 13:15:42 -0700

    uncommented location.reload in the .then function of ajax put

--- public/assets/javascript/burgers.js
+++ public/assets/javascript/burgers.js
@@ -9,7 +9,7 @@ $(function() {
 
     $.ajax("/api/burgers/" + id, { type: "PUT", data: devouredState }).then(function() {
       console.log("changed devoured to", isDevoured);
-      // location.reload();
+      location.reload();
     });
   });
 

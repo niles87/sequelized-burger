fc110782a62991679762261645a88c73a5b057fc
niles87 <nbing19@gmail.com>
2020-03-18 06:17:27 -0700

    edited heroku connection

--- models/index.js
+++ models/index.js
@@ -9,12 +9,12 @@ const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else if (process.env.DATABASE_URL) {
+if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "mysql",
   });
+} else if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }

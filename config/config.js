49b014dc8013b69737ce124845d8ab9787521f07
niles87 <nbing19@gmail.com>
2020-03-17 20:06:14 -0700

    initial commit using a js file for config instead of json

---
+++ config/config.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const fs = require("fs");
+
+module.exports = {
+  development: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    port: 3306,
+    dialect: "mysql",
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
+  },
+  test: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    port: 3306,
+    dialect: "mysql",
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    port: process.env.PROD_DB_PORT,
+    dialect: "mysql",
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
+  },
+};

bc8772febf1e4fb96d51eb9c67365a67bd017eaa
niles87 <nbing19@gmail.com>
2020-03-18 18:13:18 -0700

    testing

--- config/config.js
+++ config/config.js
@@ -26,11 +26,12 @@ module.exports = {
     },
   },
   production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME,
-    host: process.env.PROD_DB_HOSTNAME,
-    port: process.env.PROD_DB_PORT,
+    use_env_variable: JAWSDB_URL,
+    // username: process.env.PROD_DB_USERNAME,
+    // password: process.env.PROD_DB_PASSWORD,
+    // database: process.env.PROD_DB_NAME,
+    // host: process.env.PROD_DB_HOSTNAME,
+    // port: process.env.PROD_DB_PORT,
     dialect: "mysql",
   },
 };
